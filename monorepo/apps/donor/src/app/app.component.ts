c04e8ed48cd3c141ed968ca1542ef918c464f7f3
Pave Novoseltsev <pnovoseltse@iponweb.net>
2022-03-02 06:47:11 +0300

    md + menu

--- monorepo/apps/donor/src/app/app.component.ts
+++ monorepo/apps/donor/src/app/app.component.ts
@@ -1,4 +1,31 @@
 import { Component } from '@angular/core';
+import { NestedTreeControl } from "@angular/cdk/tree";
+import {MatTreeNestedDataSource} from "@angular/material/tree";
+
+interface Node {
+  name: string;
+  children?: Node[];
+}
+
+const TREE_DATA: Node[] = [
+  {
+    name: 'Fruit',
+    children: [{name: 'Apple'}, {name: 'Banana'}, {name: 'Fruit loops'}],
+  },
+  {
+    name: 'Vegetables',
+    children: [
+      {
+        name: 'Green',
+        children: [{name: 'Broccoli'}, {name: 'Brussels sprouts'}],
+      },
+      {
+        name: 'Orange',
+        children: [{name: 'Pumpkins'}, {name: 'Carrots'}],
+      },
+    ],
+  },
+];
 
 @Component({
   selector: 'monorepo-root',
@@ -6,5 +33,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+  }
   title = 'donor';
+  dataSource = new MatTreeNestedDataSource<Node>();
+  treeControl = new NestedTreeControl<Node>(node => node.children);
+  hasChild = (_: number, node: Node) => !!node.children && node.children.length > 0;
 }
--- monorepo/apps/donor/src/app/app.module.ts
+++ monorepo/apps/donor/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from "@angular/material/icon";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatButtonModule } from "@angular/material/button";
+import { MatTreeModule } from "@angular/material/tree";
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatTreeModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
