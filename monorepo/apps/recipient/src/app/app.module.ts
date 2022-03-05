4e94b17384bd875e3cfa0bade47f21ab32ee9c6b
Pave Novoseltsev <pnovoseltse@iponweb.net>
2022-03-05 09:52:23 +0300

    form validation

--- monorepo/apps/recipient/src/app/app.module.ts
+++ monorepo/apps/recipient/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppRoutingModule } from "./app.routing.module";
 import {Entity1FeatureResolverService} from "./entity1/services/entity1-feature-resolver.service";
 import {RecipientFeature1Module} from "./entity1/features/recipient-feature1/recipient-feature1.module";
 import {Entity1ColumnFeatureResolverService} from "./entity1/services/entity1-column-feature-resolver.service";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +17,8 @@ import {Entity1ColumnFeatureResolverService} from "./entity1/services/entity1-co
     BrowserAnimationsModule,
     AppRoutingModule,
     SharedModule,
-    RecipientFeature1Module
+    RecipientFeature1Module,
+    HttpClientModule,
   ],
   providers: [
     {provide: ENTITY1_FEATURE_RESOLVER, useClass: Entity1FeatureResolverService},
